87026f74c53b741c70ec49c9874f5fc60791af07
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2021-02-17 23:43:10 +0100

    #12: convert sanitizer executable into plugin

--- src/sanitizer/sanitizer.cc
+++ src/sanitizer/sanitizer.cc
@@ -54,6 +54,7 @@
 
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
 
 #include <memory>
 
@@ -67,10 +68,9 @@ using namespace llvm;
 using namespace clang::tooling;
 using namespace clang::ast_matchers;
 
-static FILE* out = nullptr;
+static FILE* out = stdout;
 
 static cl::opt<std::string> Filename("o", cl::desc("Filename to output generated code"));
-static cl::list<std::string> Includes("I", cl::desc("Include directories"), cl::ZeroOrMore);
 static cl::opt<bool> GenerateInline("inline", cl::desc("Generate code inline and modify files"));
 static cl::opt<bool> OutputMainFile("include-input", cl::desc("Output input file with generated code"));
 static cl::opt<bool> IncludeVTHeader("Ivt", cl::desc("Include VT headers in generated code"));
@@ -147,46 +147,55 @@ private:
   Rewriter rw_;
 };
 
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static cl::OptionCategory SerializeCheckerCategory("Serialize sanitizer");
-
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-
 // A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nGenerates sanitizer code for serializers\n");
 
-int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, SerializeCheckerCategory);
+// int main(int argc, const char **argv) {
+//   if (Filename == "") {
+//     out = stdout;
+//   } else {
+//     out = fopen(Filename.c_str(), "w");
+//   }
 
-  ClangTool Tool(
-    OptionsParser.getCompilations(), OptionsParser.getSourcePathList()
-  );
+//   if (IncludeVTHeader) {
+//     fmt::print(out, "#include <vt/transport.h>\n");
+//   }
 
-  if (Filename == "") {
-    out = stdout;
-  } else {
-    out = fopen(Filename.c_str(), "w");
+//   for (auto&& e : Includes) {
+//     auto str = std::string("-I") + e;
+//     ArgumentsAdjuster ad1 = getInsertArgumentAdjuster(str.c_str());
+//     Tool.appendArgumentsAdjuster(ad1);
+//     fmt::print(stderr, "Including {}\n", e);
+//   }
+
+//   Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+
+//   if (Filename == "" and out != nullptr) {
+//     fclose(out);
+//   }
+//   return 0;
+// }
+
+struct SanitizerPluginAction : public PluginASTAction {
+public:
+  bool ParseArgs(CompilerInstance const&,
+                 std::vector<std::string> const& args) override {
+    for (auto&& arg : args) {
+      llvm::errs() << arg;
+    }
+    return true;
   }
 
-  if (IncludeVTHeader) {
-    fmt::print(out, "#include <vt/transport.h>\n");
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance&,
+                                                 StringRef) override {
+    return std::make_unique<MyASTConsumer>(rw_);
   }
 
-  for (auto&& e : Includes) {
-    auto str = std::string("-I") + e;
-    ArgumentsAdjuster ad1 = getInsertArgumentAdjuster(str.c_str());
-    Tool.appendArgumentsAdjuster(ad1);
-    fmt::print(stderr, "Including {}\n", e);
-  }
+private:
+  Rewriter rw_;
+};
 
-  Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
-
-  if (Filename == "" and out != nullptr) {
-    fclose(out);
-  }
-  return 0;
-}
+// register the plugin
+static FrontendPluginRegistry::Add<SanitizerPluginAction>
+    X(/*Name=*/"sanitizer",
+      /*Desc=*/"Serialization Sanitizer");
